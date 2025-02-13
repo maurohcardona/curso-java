1f923bcd72fa84624fb8f1bd3505075229808af0
Mauro Cardona <maurohcardona@gmail.com>
2025-02-13 17:30:22 -0300

    Banco

---
+++ Primeros Pasos/src/programacion_concurrente/threads/BancoSinSincronizar.java
@@ -0,0 +1,80 @@
+package programacion_concurrente.threads;
+
+import java.util.concurrent.locks.Lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BancoSinSincronizar {
+    public static void main(String[] args) {
+
+        Banco b = new Banco();
+
+        for (int i = 0; i < 100; i++) {
+            EjecucionTransferencias r = new EjecucionTransferencias(b, i, 2000);
+            Thread t = new Thread(r);
+            t.start();
+        }
+        
+    }
+}
+
+class Banco {
+
+    public Banco() {
+        cuentas = new double[100];
+        for (int i = 0; i < cuentas.length; i++) {
+            cuentas[i] = 2000;
+        }
+    }
+
+    public void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) {
+        cerrojo.lock();
+        if (cuentas[cuentaOrigen] < cantidad) return;
+        System.out.println(Thread.currentThread());
+        cuentas[cuentaOrigen] -= cantidad;
+        System.out.printf("%10.2f de %d para %d ", cantidad, cuentaOrigen, cuentaDestino);
+        cuentas[cuentaDestino] += cantidad;
+        System.out.printf("Saldo total: %10.2f%n", getSaldoTotal());
+        cerrojo.unlock();
+    }
+
+    public double getSaldoTotal() {
+
+        double sumaCuentas = 0;
+        for (double a : cuentas) {
+            sumaCuentas += a;
+        }
+        return sumaCuentas;
+    }
+
+    private final double[] cuentas;
+
+    private Lock cerrojo = new ReentrantLock();
+}
+
+class EjecucionTransferencias implements Runnable {
+
+    private Banco banco;
+    private int deLaCuenta;
+    private double cantidadMax;
+
+    public EjecucionTransferencias(Banco b, int de, double max) {
+        banco = b;
+        deLaCuenta = de;
+        cantidadMax = max;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                int paraLaCuenta = (int) (100 * Math.random());
+                double cantidad = cantidadMax * Math.random();
+                banco.transferencia(deLaCuenta, paraLaCuenta, cantidad);
+                Thread.sleep(5000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
