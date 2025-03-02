82925774b23b0286696e3f1e64d2f07e5eb3c26c
Mauro Cardona <maurohcardona@gmail.com>
2025-03-02 14:17:29 -0300

    Chat

--- Primeros Pasos/src/sockets/juan_sockects/Cliente.java
+++ Primeros Pasos/src/sockets/juan_sockects/Cliente.java
@@ -1,8 +1,25 @@
 package sockets.juan_sockects;
 
 import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.ServerSocket;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import javax.swing.text.StyledDocument;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+
+
+import java.io.Serializable;
 
 
 public class Cliente {
@@ -30,39 +47,242 @@ class MarcoCliente extends JFrame{
 		add(milamina);
 		
 		setVisible(true);
-		}	
+
+		addWindowListener(new EnvioOnline());
+	}	
 	
 }
 
-class LaminaMarcoCliente extends JPanel{
+class EnvioOnline extends WindowAdapter{
+	
+	public void windowOpened(WindowEvent e) {
+		
+		try {
+			
+			Socket misocket = new Socket("192.168.100.235",9999);
+
+			PaqueteEnvio datos = new PaqueteEnvio(" online", "192.168.100.235", "192.168.100.235");
+			
+			ObjectOutputStream paquete_datos = new ObjectOutputStream(misocket.getOutputStream());
+
+			paquete_datos.writeObject(datos);
+
+			misocket.close();
+		}
+		catch (UnknownHostException e1) {
+			
+			e1.printStackTrace();
+		}
+		catch (IOException e1) {
+			
+			System.out.println(e1.getMessage());
+		}
+	}	
+}
+
+class LaminaMarcoCliente extends JPanel implements Runnable{
 	
 	public LaminaMarcoCliente(){
+
+		String nickNames = JOptionPane.showInputDialog("Elije tu NickName");
 	
-		JLabel texto=new JLabel("CLIENTE");
+		JLabel texto=new JLabel(" Online: ");
+
+		nickName = new JLabel(nickNames);
+
+		ip = new JComboBox<>();
+
+		ip.addItem("Loli");
+
+		ip.addItem("Mauro");
+
+		ip.addItem("Pedro");
+
+		add(new JLabel("Nick: "));
+
+		add(nickName);
 		
 		add(texto);
+
+		add(ip);
+
+		campo_chat = new JTextPane();
+
+		campo_chat.setPreferredSize(new Dimension(240, 200));
+
+		campo_chat.setEditable(false);
+
+		JScrollPane scrollPane = new JScrollPane(campo_chat);
+
+		scrollPane.setPreferredSize(new Dimension(240, 200)); 
+
+		scrollPane.setMinimumSize(new Dimension(240, 150));
+
+		add(campo_chat);
 	
-		campo1=new JTextField(20);
+		mensaje=new JTextField(24);
 	
-		add(campo1);		
+		add(mensaje);		
 	
 		miboton=new JButton("Enviar");
 
 		miboton.addActionListener(new EnviaTexto());
 		
 		add(miboton);	
+
+		Thread miHilo = new Thread(this);
+
+		miHilo.start();
 		
 	}	
-	private JTextField campo1;
+	private JTextField mensaje;
+
+	private JLabel nickName;
+
+	private JComboBox<String> ip;
+
+	private JTextPane campo_chat;
 	
 	private JButton miboton;
+
+	public void run(){
+		
+		try {
+			
+			ServerSocket servidor = new ServerSocket(9090);
+			
+			Socket cliente;
+			
+			PaqueteEnvio paquete;
+
+			while (true) {
+				
+				cliente = servidor.accept();
+				
+				ObjectInputStream flujo_entrada = new ObjectInputStream(cliente.getInputStream());
+				
+				paquete = (PaqueteEnvio) flujo_entrada.readObject();
+
+				StyledDocument doc = campo_chat.getStyledDocument();
+
+				SimpleAttributeSet otherStyle = new SimpleAttributeSet();
+
+        		StyleConstants.setForeground(otherStyle, Color.RED);  
+				
+				doc.insertString(doc.getLength(), "\n" + paquete.getNickName() + ": " + paquete.getMensaje(), otherStyle);
+				
+				cliente.close();
+			}	
+			
+		} catch (Exception e) {
+			
+			System.out.println(e.getMessage());
+
+		}
+	}
 
 	private class EnviaTexto implements ActionListener{
 		
 		public void actionPerformed(ActionEvent e) {
+
+			StyledDocument doc = campo_chat.getStyledDocument();
+
+			SimpleAttributeSet userStyle = new SimpleAttributeSet();
+
+        	StyleConstants.setForeground(userStyle, Color.BLUE);  
+
 			
-			System.out.println(campo1.getText());
+			try {
+				doc.insertString(doc.getLength(), "\n" + nickName.getText() + ": " + mensaje.getText(), userStyle);
+				
+				Socket misocket = new Socket("192.168.100.235",9999);
+
+				PaqueteEnvio datos = new PaqueteEnvio(mensaje.getText(), nickName.getText(), ip.getSelectedItem().toString());
+
+				ObjectOutputStream paquete_datos = new ObjectOutputStream(misocket.getOutputStream());	
+
+				paquete_datos.writeObject(datos);
+
+				mensaje.setText("");
+
+				//DataOutputStream flujo_salida = new DataOutputStream(misocket.getOutputStream());
+				// flujo_salida.writeUTF(mensaje.getText());
+				// mensaje.setText("");
+				// flujo_salida.close();
+
+				misocket.close();
+
+			} catch (UnknownHostException e1) {
+				
+				e1.printStackTrace();
+
+			} catch (Exception e1) {
+				
+				System.out.println(e1.getMessage());
+
+			} 
+			
 		}
+	}
+	
+}
+
+class PaqueteEnvio implements Serializable{
+	
+	private String mensaje, nickName, ip;
+	
+	public PaqueteEnvio(String mensaje, String nickName, String ip) {
+		
+		this.mensaje = mensaje;
+		
+		this.nickName = nickName;
+
+		this.ip = ip;
+		
+	}
+	
+	public String getMensaje() {
+		
+		return mensaje;
+		
+	}
+
+	public void setMensaje(String mensaje) {
+		
+		this.mensaje = mensaje;
+		
+	}
+	
+	public String getNickName() {
+		
+		return nickName;
+		
+	}
+
+	public void setNickName(String nickName) {
+		
+		this.nickName = nickName;
+		
+	}
+
+	public String getIp() {
+		
+		switch (ip) {
+			case "Loli":
+				return "192.168.100.232";
+			case "Mauro":
+				return "192.168.100.235";
+			case "Pedro":
+				return "192.168.100.234";
+			default:
+				return "";
+		}
+	}
+
+	public void setIp(String ip) {
+		
+		this.ip = ip;
+		
 	}
 	
 }
--- Primeros Pasos/src/sockets/juan_sockects/Servidor.java
+++ Primeros Pasos/src/sockets/juan_sockects/Servidor.java
@@ -3,6 +3,13 @@ package sockets.juan_sockects;
 import javax.swing.*;
 
 import java.awt.*;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 
 public class Servidor  {
 
@@ -16,7 +23,7 @@ public class Servidor  {
 	}	
 }
 
-class MarcoServidor extends JFrame {
+class MarcoServidor extends JFrame implements Runnable{
 	
 	public MarcoServidor(){
 		
@@ -33,8 +40,69 @@ class MarcoServidor extends JFrame {
 		add(milamina);
 		
 		setVisible(true);
+
+		Thread mihilo = new Thread(this);
+
+		mihilo.start();
 		
 		}
 	
 	private	JTextArea areatexto;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Estoy a la escucha");
+
+		try {
+
+			ServerSocket servidor = new ServerSocket(9999);
+
+			String nick, ip, mensaje;	
+
+			PaqueteEnvio paquete_recibido;
+
+			while (true) {
+				Socket misocket = servidor.accept();
+
+				InetAddress localizacion = misocket.getInetAddress();
+
+				String ipRemota = localizacion.getHostAddress();
+
+				System.out.println("Conexión entrante desde la IP: " + ipRemota);
+
+				ObjectInputStream paquete_datos = new ObjectInputStream(misocket.getInputStream());
+
+				paquete_recibido = (PaqueteEnvio) paquete_datos.readObject();
+
+				nick = paquete_recibido.getNickName();
+
+				ip = paquete_recibido.getIp();
+				
+				System.out.println(ip);
+
+				mensaje = paquete_recibido.getMensaje();
+
+				areatexto.append("\n" + nick + ": " + mensaje + " para " + ip);
+
+				Socket enviaDestinatario = new Socket(ip, 9090);
+
+				ObjectOutputStream paqueteReenvio = new ObjectOutputStream(enviaDestinatario.getOutputStream());
+
+				paqueteReenvio.writeObject(paquete_recibido);
+
+				paqueteReenvio.close();
+
+				enviaDestinatario.close();
+
+				misocket.close();
+
+			}
+
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+	}
 }
